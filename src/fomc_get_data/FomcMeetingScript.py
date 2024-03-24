ccc2566fb677154e9462d61ab138263c1430d508
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-03-24 01:33:48 -0400

    updates for FOMC meeting Script

--- src/fomc_get_data/FomcMeetingScript.py
+++ src/fomc_get_data/FomcMeetingScript.py
@@ -12,3 +12,23 @@ from bs4 import BeautifulSoup
 from datatime import datetime
 
 from .FomcBase import FomcBase
+
+
+class FomcMeetingScript(FomcBase):
+    """
+    A convenient class for extracting meeting scripts from the FOMC website.
+    FOMC publishes the meeting scripts after 5 years, so this cannot be used
+    for the prediction of the monetary policy in real-time.
+
+    Example Usage:
+        fomc = FomcMeetingScript()
+        df = fomc.get_contents()
+    """
+
+    def __init__(self, verbose=True, max_threads=10, base_dir="../data/FOMC/"):
+        super().__init__("meeting_script", verbose, max_threads, base_dir)
+
+    def _get_links(self, from_year):
+        """
+        Override private function
+        """
